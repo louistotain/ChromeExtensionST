02bf9617c7031cf4af94b1c5013889e521895e0e
louistotain <louistotain@gmail.com>
2021-10-19 17:54:54 +0200

    mode reload apres changement de page

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ htmlObject2.style.borderRadius = "5px";
 htmlObject2.style.padding = "5px";
 htmlObject2.style.margin = "5px";
 htmlObject2.onclick = function(e) {
-  window.location.replace("http://192.168.102.21/social_clean/html/");
+  window.location.replace("http://socialclean");
 };
 
 var p2 = "<span id='ChromeExtRetour' style='font-size: 15px; font-weight: bold; color: white; padding: 5px; text-shadow: 3px 3px 3px #0000009e; cursor: pointer' >Retour</span>";
@@ -66,7 +66,7 @@ if (document.getElementsByClassName('navbar').length){
   document.getElementsByClassName('region--content').item(0).style.minWidth = "100%";
   document.getElementsByClassName('region--content').item(0).style.paddingRight = "0px";
 
-  document.getElementsByTagName('body').item(0).style.backgroundImage = "url(http://192.168.102.21/social_clean/html/themes/contrib/socialtremblaye1/assets/images/background_tremblaye_base_4.jpg)";
+  document.getElementsByTagName('body').item(0).style.backgroundImage = "url(http://socialclean/themes/contrib/socialtremblaye1/assets/images/background_tremblaye_base_4.jpg)";
   document.getElementsByTagName('body').item(0).style.backgroundSize = "cover";
 
 }
--- js/app.js
+++ js/app.js
@@ -1,8 +1,44 @@
-var time = 60;
+// var time = 60;
+//
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//
+//     if (changeInfo.status == 'complete') {
+//
+//         // Get a reference to the last interval + 1
+//         const interval_id = window.setInterval(function () {
+//         }, Number.MAX_SAFE_INTEGER);
+//
+//         // Clear any timeout/interval up to that id
+//         for (let i = 1; i < interval_id; i++) {
+//             window.clearInterval(i);
+//         }
+//
+//         var interval = setInterval(function () {
+//
+//             if (time == 0) {
+//                 chrome.tabs.update(undefined, {url: "http://socialclean"});
+//                 time = 60;
+//                 clearInterval(interval);
+//             } else {
+//                 time = time - 1;
+//
+//                 chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+//                     chrome.tabs.sendMessage(tabs[0].id, {data: time});
+//                 });
+//             }
+//
+//         }, 1000);
+//
+//     }
+//
+// });
+
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
     if (changeInfo.status == 'complete') {
+
+        var time = 60;
 
         // Get a reference to the last interval + 1
         const interval_id = window.setInterval(function () {
@@ -16,7 +52,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         var interval = setInterval(function () {
 
             if (time == 0) {
-                chrome.tabs.update(undefined, {url: "http://192.168.102.21/social_clean/html/"});
+                chrome.tabs.update(undefined, {url: "http://socialclean"});
                 time = 60;
                 clearInterval(interval);
             } else {
