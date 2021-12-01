fe3d7743fdd2de834e7b2060c2c3c2b516fccb4a
louistotain <louistotain@gmail.com>
2021-12-01 09:07:28 +0100

    add new link + change body image

--- js/app.js
+++ js/app.js
@@ -1,39 +1,3 @@
-// var time = 60;
-//
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-//
-//     if (changeInfo.status == 'complete') {
-//
-//         // Get a reference to the last interval + 1
-//         const interval_id = window.setInterval(function () {
-//         }, Number.MAX_SAFE_INTEGER);
-//
-//         // Clear any timeout/interval up to that id
-//         for (let i = 1; i < interval_id; i++) {
-//             window.clearInterval(i);
-//         }
-//
-//         var interval = setInterval(function () {
-//
-//             if (time == 0) {
-//                 chrome.tabs.update(undefined, {url: "http://socialclean"});
-//                 time = 60;
-//                 clearInterval(interval);
-//             } else {
-//                 time = time - 1;
-//
-//                 chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-//                     chrome.tabs.sendMessage(tabs[0].id, {data: time});
-//                 });
-//             }
-//
-//         }, 1000);
-//
-//     }
-//
-// });
-
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
     if (changeInfo.status == 'complete') {
@@ -52,7 +16,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         var interval = setInterval(function () {
 
             if (time == 0) {
-                chrome.tabs.update(undefined, {url: "http://socialclean"});
+                chrome.tabs.update(undefined, {url: "https://socialtest.tremblaye-sa.fr/ecran-dynamique"});
                 time = 60;
                 clearInterval(interval);
             } else {
